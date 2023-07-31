fbf69f2300c77d02c473975165e517e04a2a2671
Anagho <anaghosamroyce@gmail.com>
2023-07-31 14:23:37 +0100

    Updated Gig page

--- fiverr-ui/src/components/navbar/Navbar.jsx
+++ fiverr-ui/src/components/navbar/Navbar.jsx
@@ -1,13 +1,12 @@
-import { useEffect, useState } from "react";
-import "./Navbar.scss";
+import React, { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
+import "./Navbar.scss";
 
-const Navbar = () => {
+function Navbar() {
   const [active, setActive] = useState(false);
   const [open, setOpen] = useState(false);
 
-  // Using the useLocation hook to maintain the navbar in other pages
-  const {pathname} = useLocation()
+  const { pathname } = useLocation();
 
   const isActive = () => {
     window.scrollY > 0 ? setActive(true) : setActive(false);
@@ -15,69 +14,109 @@ const Navbar = () => {
 
   useEffect(() => {
     window.addEventListener("scroll", isActive);
-
     return () => {
       window.removeEventListener("scroll", isActive);
     };
   }, []);
 
+  // const currentUser = null
+
   const currentUser = {
     id: 1,
-    username: "Emma Anagho",
+    username: "Emmy",
     isSeller: true,
   };
+
   return (
-    <div className={active || pathname!=="/" ? "navbar active" : "navbar"}>
+    <div className={active || pathname !== "/" ? "navbar active" : "navbar"}>
       <div className="container">
         <div className="logo">
-          <Link to="/" className="link">
-          <span className="text">fiverr</span>
+          <Link className="link" to="/">
+            <span className="text">Fiverr</span>
           </Link>
           <span className="dot">.</span>
         </div>
         <div className="links">
-          <span>Fiverr Business</span>
+          <span>Liverr Business</span>
           <span>Explore</span>
           <span>English</span>
-          <span>Sign in</span>
           {!currentUser?.isSeller && <span>Become a Seller</span>}
-          {!currentUser && <button>Join</button>}
-          {currentUser && (
-            <div className="user" onClick={() => setOpen(!open)}>
+          {currentUser ? (
+            <div className="user" onClick={()=>setOpen(!open)}>
               <img
-                src="https://fiverr-res.cloudinary.com/t_profile_thumb,q_auto,f_auto/attachments/profile/photo/24fce8d90e2e21d2153501b23b70a996-1675703231121/263aa290-2cd3-414f-8216-f847c87c4702.png"
-                alt="profile"
+                src="https://images.pexels.com/photos/1115697/pexels-photo-1115697.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
               />
               <span>{currentUser?.username}</span>
-              {open && (
-                <div className="options">
-                  {currentUser?.isSeller && (
-                    <>
-                      <Link className="link" to="/mygigs">Gigs</Link>
-                      <Link className="link" to="/add">Add New Gig</Link>
-                    </>
-                  )}
-                  <Link className="link" to="/orders">Orders</Link>
-                  <Link className="link" to="/messages">Messages</Link>
-                  <Link className="link" to="/">Logout</Link>
-                </div>
-              )}
+              {open && <div className="options">
+                {currentUser.isSeller && (
+                  <>
+                    <Link className="link" to="/mygigs">
+                      Gigs
+                    </Link>
+                    <Link className="link" to="/add">
+                      Add New Gig
+                    </Link>
+                  </>
+                )}
+                <Link className="link" to="/orders">
+                  Orders
+                </Link>
+                <Link className="link" to="/messages">
+                  Messages
+                </Link>
+                <Link className="link" to="/">
+                  Logout
+                </Link>
+              </div>}
             </div>
+          ) : (
+            <>
+              <span>Sign in</span>
+              <Link className="link" to="/register">
+                <button>Join</button>
+              </Link>
+            </>
           )}
         </div>
       </div>
-
-      {active || pathname !=="/" && (
+      {(active || pathname !== "/") && (
         <>
           <hr />
           <div className="menu">
-            <Link>Test</Link>
-            <Link>Test2</Link>
+            <Link className="link menuLink" to="/">
+              Graphics & Design
+            </Link>
+            <Link className="link menuLink" to="/">
+              Video & Animation
+            </Link>
+            <Link className="link menuLink" to="/">
+              Writing & Translation
+            </Link>
+            <Link className="link menuLink" to="/">
+              AI Services
+            </Link>
+            <Link className="link menuLink" to="/">
+              Digital Marketing
+            </Link>
+            <Link className="link menuLink" to="/">
+              Music & Audio
+            </Link>
+            <Link className="link menuLink" to="/">
+              Programming & Tech
+            </Link>
+            <Link className="link menuLink" to="/">
+              Business
+            </Link>
+            <Link className="link menuLink" to="/">
+              Lifestyle
+            </Link>
           </div>
+          <hr />
         </>
       )}
     </div>
   );
-};
+}
 
 export default Navbar;
